501ea43d93d959706e4e29ed4878927716d9fe85
404BySam <devbysam404@gmail.com>
2025-06-30 16:15:57 +0200

    Map and Maker add with API

--- components/DetailCity.js
+++ components/DetailCity.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, Text, View, StyleSheet, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export function DetailCity({ latitude, longitude }) {
-  const [isLoading, setisLoading] = useState();
+  const [isLoading, setIsLoading] = useState(true); // pour l'afficher en dur le passer en paramètre true
   const [weather, setWeather] = useState(null);
   const API_KEY = "71db9d95bd3d3945ea779289ed542768";
 
@@ -17,7 +18,7 @@ export function DetailCity({ latitude, longitude }) {
       .then((data) => setWeather(data));
   }, [latitude, longitude]);
 
-  if (!weather) return <ActivityIndicator size="small" color="#0000ff" />;
+  if (!weather) return <ActivityIndicator />;
 
   const iconApi = weather.weather?.[0]?.icon;
   const iconUrl = `https://openweathermap.org/img/wn/${iconApi}@2x.png`;
@@ -25,16 +26,26 @@ export function DetailCity({ latitude, longitude }) {
   return (
     <>
       {isLoading == true ? (
-        <ActivityIndicator />
+        <ActivityIndicator size="large" color="blue" />
       ) : (
         <View style={styles.container}>
           <Text>Ville : {weather.name}</Text>
           <Text>Température : {weather.main?.temp}°C</Text>
           <Text>Description : {weather.weather?.[0]?.description}</Text>
-          <Image
-            source={{ uri: iconUrl }}
-            style={{ width: 80, height: 80 }} // Ajuste la taille si tu veux
-          />
+          <Image source={{ uri: iconUrl }} style={{ width: 80, height: 80 }} />
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: latitude,
+              longitude: longitude,
+            }}
+          >
+            <Marker
+              coordinate={{ latitude: latitude, longitude: longitude }}
+              title={weather.name}
+              description={weather.weather?.[0]?.description}
+            />
+          </MapView>
         </View>
       )}
     </>
@@ -44,5 +55,11 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     justifyContent: "center",
+  },
+  map: {
+    width: 200,
+    height: 220,
+    borderRadius: 100,
+    marginTop: 16,
   },
 });
